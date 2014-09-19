ed1eeb3c474b5d2b093842697a29be165f1b6fb4
Emin <emintham@gmail.com>
2014-09-19 08:22:28 -0700

    extracts card model to common

---
+++ common/models.py
@@ -0,0 +1,42 @@
+from mongoengine import *
+
+# Create your models here.
+class AbstractCard(Document):
+	advancementcost = IntField()
+	agendapoints = IntField()
+	baselink = IntField()
+	card_type = StringField()
+	code = StringField()
+	cost = IntField()
+	cyclenumber = IntField()
+	faction = StringField()
+	faction_code = StringField()
+	faction_letter = StringField()
+	faction_cost = IntField()
+	flavor = StringField()
+	illustrator = StringField()
+	influence_limit = IntField()
+	limited = BooleanField()
+	memoryunits = IntField()
+	minimumdecksize = IntField()
+	number = IntField()
+	set_code = StringField()
+	setname = StringField()
+	side = StringField()
+	side_code = StringField()
+	strength = IntField()
+	subtype = StringField()
+	subtype_code = StringField()
+	text = StringField()
+	title = StringField()
+	type_code = StringField()
+	uniqueness = BooleanField()
+	meta = {
+		'abstract': True,
+		'ordering': ['code']
+	}
+
+class Card(AbstractCard):
+	meta = {
+		'collection': 'cards',
+	}
